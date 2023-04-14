e9278903b2299d18f28b4f38e2b11a3320ab543a
ravisavaliya72 <ravi.savaliya72@gmail.com>
2023-04-14 11:51:31 +0530

    done

--- app/src/main/java/com/trip/myproject/AboutActivity.java
+++ app/src/main/java/com/trip/myproject/AboutActivity.java
@@ -3,17 +3,18 @@ package com.trip.myproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
-
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
         String data = getIntent().getStringExtra("data");
-        TextView about = findViewById(R.id.tripData);
-        about.setText(data);
+        WebView about = findViewById(R.id.tripData);
+        about.loadUrl(data);
     }
 }
